c080239c6d4fbb221ca03406ce656d5419c7e379
Iosif Nicolae <iosif@bringes.io>
2024-05-20 12:02:19 +0300

    feat: add long_description

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+this_directory = Path(__file__).parent
+
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="django_superapp",
     packages=find_packages(where="src"),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     package_dir={"": "src"},
     install_requires=[
         "Django >= 5.0.0",
